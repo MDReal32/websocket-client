618906ab07b6951487de07a6b48151fb2e6d288e
MDReal32 <veysaliyev00@gmail.com>
2023-02-28 08:27:41 -0500

    Build config changed

--- scripts/esbuild.config.ts
+++ scripts/esbuild.config.ts
@@ -1,31 +1,56 @@
 import { build, BuildOptions } from "esbuild";
+import { writeFileSync } from "fs";
 
-const isProduction = process.env.NODE_ENV === "production";
-
-const nodeConfig: BuildOptions = {
-  platform: "node",
+const baseBuildOptions: BuildOptions = {
   entryPoints: ["./src/websocket-client.ts"],
-  bundle: true,
-  external: ["ws"],
-  outfile: "build/node.js",
   target: "es6",
-  format: "cjs",
-  minify: isProduction,
-  sourcemap: !isProduction
+  bundle: true
 };
 
-const browserConfig: BuildOptions = {
+const nodeBuildOption: BuildOptions = {
+  ...baseBuildOptions,
+  platform: "node",
+  external: ["ws"],
+  outfile: "build/node.${env}.js",
+  format: "cjs"
+};
+
+const browserBuildOption: BuildOptions = {
+  ...baseBuildOptions,
   platform: "browser",
-  entryPoints: ["./src/websocket-client.ts"],
-  bundle: true,
-  outfile: "build/browser.mjs",
-  target: "es6",
+  outfile: "build/browser.${env}.mjs",
   format: "esm",
-  minify: isProduction,
-  sourcemap: !isProduction,
   define: {
     "process.env.NODE_ENV": `"${process.env.NODE_ENV || "development"}"`
   }
 };
 
-Promise.all([build(browserConfig), build(nodeConfig)]).catch(() => process.exit(1));
+const nodeBuildOptions = [true, false].map(env => ({
+  ...nodeBuildOption,
+  outfile: nodeBuildOption.outfile!.replace("${env}", env ? "min" : "dev"),
+  minify: env,
+  sourcemap: !env
+}));
+const browserBuildOptions = [true, false].map(env => ({
+  ...browserBuildOption,
+  outfile: browserBuildOption.outfile!.replace("${env}", env ? "min" : "dev"),
+  minify: env,
+  sourcemap: !env
+}));
+
+Promise.all([...nodeBuildOptions, ...browserBuildOptions].map(options => build(options)))
+  .then(() => {
+    const code = `if (process.env.NODE_ENV === "development") {
+  module.exports = require("./\${name}.dev.mjs");
+} else {
+  module.exports = require("./\${name}.min.mjs");
+}
+`;
+
+    ["browser.mjs", "node.js"].forEach(name => {
+      const compiledCode = code.replace(/\${name}/g, name.split(".")[0]);
+      const path = `./build/${name}`;
+      writeFileSync(path, compiledCode);
+    });
+  })
+  .catch(() => process.exit(1));
